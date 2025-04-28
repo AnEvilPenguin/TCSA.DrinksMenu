a41a98963f7754cc7e7a400155757c3b9aed6fdb
Dave Lewis <siu06dl@gmail.com>
2025-04-28 18:13:19 +0100

    feat: tutorial code

---
+++ Drinks Menu/Drinks Menu/Repository.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Drinks_Menu;
+public record Repository(
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("description")] string Description,
+    [property: JsonPropertyName("html_url")] Uri GitHubHomeUrl,
+    [property: JsonPropertyName("homepage")] Uri Homepage,
+    [property: JsonPropertyName("watchers")] int Watchers);
